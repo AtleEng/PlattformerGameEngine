0123928f0aa047725132fbd0f98a904ed1806d4c
AtleEng <atle.engelbrektsson@elev.ga.ntig.se>
2023-10-13 09:49:11 +0200

    Start

---
+++ Plattformer/Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Numerics;
+using System.Collections.Generic;
+using Raylib_cs;
+using CoreEngine;
+
+namespace Engine
+{
+    public class PlayerMovement : Component, IScript
+    {
+        public override void Start()
+        {
+
+        }
+        public override void Update(float delta)
+        {
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+            {
+                gameEntity.transform.size.Y += 2f;
+            }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN))
+            {
+                gameEntity.transform.size.Y -= 2f;
+            }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT))
+            {
+                gameEntity.transform.size.X += 2f;
+            }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))
+            {
+                gameEntity.transform.size.X -= 2f;
+            }
+
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_W))
+            {
+                gameEntity.transform.position.Y -= 1f;
+            }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_S))
+            {
+                gameEntity.transform.position.Y += 1f;
+            }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_D))
+            {
+                gameEntity.transform.position.X += 1f;
+            }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_A))
+            {
+                gameEntity.transform.position.X -= 1f;
+            }
+        }
+    }
+}
